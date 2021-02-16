26213fb79e36ea0bb8968f93d1dc4d34d62db29f
Adam Pitchie <adampitchie@gmail.com>
2021-02-16 14:19:50 -0800

    finished adding logic to game function

--- index.js
+++ index.js
@@ -162,9 +162,22 @@ Use the game function below to do the following:
   HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
 
-var choice = Math.random()
 function game(user, computer){
-    /*add your code here*/
+  if (user === computer) {
+    return "it's a tie"
+  } else if (user === 'rock' && computer === 'paper') {
+    return "you lose!"
+  } else if (user === 'rock' && computer === 'scissors') {
+    return "you win!"
+  } else if (user === 'paper' && computer === 'rock') {
+    return "you win!"
+  } else if (user === 'paper' && computer === 'scissors') {
+    return "you lose!"
+  } else if (user === 'scissors' && computer === 'paper') {
+    return "you win!"
+  } else if (user === 'scissors' && computer === 'rock') {
+    return "you lose!"
+  }
 }
   
   
